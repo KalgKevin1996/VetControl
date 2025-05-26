438f22936c103cfd06083de1da2d70487666fd0a
Kevin Antonio Lopez Gomez <kalg.antonio.lopez@gmail.com>
2025-05-26 14:35:21 -0600

    primer commit

---
+++ vetcontrol/src/main/java/com/vetcontrol/VetcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.vetcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetcontrolApplication.class, args);
+	}
+
+}
