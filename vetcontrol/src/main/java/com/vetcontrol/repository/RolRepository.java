95adffedd0ea1196826339d52aff344fa845e4a9
Kevin Antonio Lopez Gomez <kalg.antonio.lopez@gmail.com>
2025-05-26 18:18:14 -0600

    fix: modificando la interface RolRepository.java

--- vetcontrol/src/main/java/com/vetcontrol/repository/RolRepository.java
+++ vetcontrol/src/main/java/com/vetcontrol/repository/RolRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RolRepository {
+public interface RolRepository extends JpaRepository<Rol, Long> {
     Optional<Rol> findByNombre(String nombre);
 }
