7728967479f971058fc453088d63f5ecc4515343
Kevin Antonio Lopez Gomez <kalg.antonio.lopez@gmail.com>
2025-06-02 18:24:44 -0600

    ultimo cambio subido antes de subir a produccion

--- vetcontrol/src/main/java/com/vetcontrol/VetcontrolApplication.java
+++ vetcontrol/src/main/java/com/vetcontrol/VetcontrolApplication.java
@@ -1,13 +1,23 @@
 package com.vetcontrol;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VetcontrolApplication {
+public class VetcontrolApplication implements CommandLineRunner {
+
+	// Inyecta el perfil activo
+	@Value("${spring.profiles.active:default}")
+	private String perfilActivo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VetcontrolApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("✅ VetControl iniciado con el perfil: " + perfilActivo.toUpperCase());
+	}
 }
